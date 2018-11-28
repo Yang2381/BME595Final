e84048631e3ee7462753fd90287f5437fae5ee71
SzuKaiYang <bbkai2857@hotmail.com.tw>
2018-11-28 17:04:46 -0500

    Some update with the plotting

--- simpsons_BEGAN.py
+++ simpsons_BEGAN.py
@@ -13,7 +13,8 @@ import torchvision.utils as vutils
 import numpy as np
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
-from torchvision.utils import save_image
+from torchvision.utils import make_grid,save_image
+import torch.nn.functional as F
 
 # Root directory for dataset
 dataroot = "./simpsons_dataset"
@@ -35,7 +36,7 @@ ngf = 64
 ndf = 16
 
 # Number of training epochs
-num_epochs = 100
+num_epochs = 1000
 
 # Learning rate for optimizers
 lr = 0.0002
@@ -60,7 +61,7 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,shuffle=
 device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 print("Device is: ",device)
 print(torch.cuda.get_device_name(torch.cuda.current_device()))
-
+print(len(dataloader))
 cuda = True if torch.cuda.is_available() else False
 
 def weights_init_normal(m):
@@ -193,40 +194,78 @@ optimizer_D = optim.Adam(discriminator.parameters(), lr = lr, betas=(0.5, 0.999)
 
 Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
+def save_checkpoint(e,filename='ckpt.tar'):
+	modle_states = {
+	'G': generator.state_dict(),
+	'D': discriminator.state_dict()
+	}
+	optim_states = {
+	'G_optim': optimizer_G.state_dict(),
+	'D_optim': optimizer_D.state_dict()
+	}
+	states = {
+	'iter': e,
+	'epoch':num_epochs,
+	'modle_states': modle_states,
+	'optim_states': optim_states
+	}
+	#file_path = dataroot.joinpath(filename)
+	file_path = os.path.join(dataroot, filename)
+	torch.save(states, file_path)
+	print("Saved Checkpoint '{}' (iter {})".format(file_path, e))
+
+def load_checkpoint(filename='ckpt.tar'):
+	file_path = dataroot.joinpath(filename)
+	if file_path.is_file():
+		checkpiont = torch.load(file_path.open('rb'))
+		generator.load_state_dict(checkpiont['modle_states']['G'])
+		discriminator.load_state_dict(checkpiont['modle_states']['D'])
+		optimizer_G.load_state_dict(checkpoint['optim_states']['G_optim'])
+		optimizer_D.load_state_dict(checkpoint['optim_states']['D_optim'])
+		e = checkpoint['e']
+		print("Load checkpoint '{} at iteration".format(file_path,e))
+	else:
+		print("Failed to loadfile")
+
 k = 0.0
 gamma = 0.75
 lambda_k = 0.001
+img_list = []
+measure_history = []
 
 for epoch in range(num_epochs):
 	for i, data in enumerate(dataloader, 0):
 		real_img = Variable(data[0]).to(device)
 
-		# -----------------
+        # -----------------
         #  Train Generator
         # -----------------
-		optimizer_G.zero_grad()
+        optimizer_G.zero_grad()
 
-		#Sample Noise Batch * Image Size
-		z = Variable(Tensor(batch_size,64)).to(device)
+        #Sample Noise Batch * Image Size
+        z = Variable(Tensor(batch_size,64)).to(device)
 
-		#Generated batch images
-		gen_imgs = generator(z)
+        #Generated batch images
+        gen_imgs = generator(z)
 
-		# Loss measures generator's ability to fool the discriminator
-		g_loss = torch.mean(torch.abs(discriminator(gen_imgs) - gen_imgs))
-		g_loss.backward()
-		optimizer_G.step()
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = torch.mean(torch.abs(discriminator(gen_imgs) - gen_imgs))
+        g_loss.backward()
+        optimizer_G.step()
 
-		# ---------------------
+        # ---------------------
         #  Train Discriminator
         # ---------------------
         optimizer_D.zero_grad()
         # Measure discriminator's ability to classify real from generated samples
         d_real = discriminator(real_img)
         d_fake = discriminator(gen_imgs.detach())
+        d_loss_real = F.l1_loss(d_real, real_img)
+        d_loss_fake = F.l1_loss(d_fake, gen_imgs.detach())
 
-        d_loss_real = torch.mean(torch.abs(d_real - real_img))
-        d_loss_fake = torch.mean(torch.abs(d_fake - gen_imgs.detach()))
+
+        #d_loss_real = torch.mean(torch.abs(d_real - real_img))
+        #d_loss_fake = torch.mean(torch.abs(d_fake - gen_imgs.detach()))
         d_loss = d_loss_real - k * d_loss_fake
 
         d_loss.backward()
@@ -237,16 +276,52 @@ for epoch in range(num_epochs):
         #----------------
 
         diff = torch.mean(gamma * d_loss_real - d_loss_fake)
-
+       	
         # Update weight term for fake samples
         k = k + lambda_k * diff.item()
-        k = min(max(k,0),1) # Constraint to interval [0, 1]
+        k = min(max(k,0.0),1.0) # Constraint to interval [0, 1]
 
-        M = (d_loss_real + torch.abs(diff)).data[0]
+        M = (d_loss_real + torch.abs(diff)).item()
+        measure_history.append(M)
+        done = epoch * len(dataloader) + i
 
         print ("[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f] -- M: %f, k: %f" % (epoch, num_epochs, i, len(dataloader),
                                                             d_loss.item(), g_loss.item(),
                                                             M, k))
         done = epoch * len(dataloader) + i
-        if done % 100 == 0:
-        	save_image(gen_imgs[:25], 'BEGAN/%d.png' % done, nrow=5, normalize=True)
+        #print(done, done %1000 == 0)
+        if i % 500 == 0 or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+        	#with torch.no_grad():
+        	#	print("Entered here")
+        	#	fake = generator(Variable(Tensor(batch_size,64)).to(device)).detach().cpu()
+          	#img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+          	save_checkpoint(i)
+  	#print(epoch%1, img_list == [])
+        
+  	if epoch%10 == 0 and img_list != None:
+		
+		print("saving images")
+		name = "BEGAN_"+str(epoch)+".png"
+		print("filename = ",name)
+		generator.eval()
+		noise = Variable(torch.randn(batch_size,64)).to(device)
+		sample = generator(noise).mul(0.5).add(0.5)
+		sample = sample.data.cpu()
+		grid = make_grid(sample, nrow = 10, padding=2, normalize=False)
+		save_image(grid, filename = name)
+		generator.train()
+
+
+		#plt.figure(1)
+		#plt.axis("off")
+		#plt.title("fake_image")
+		#print(img_list)
+		#plt.imsave(name,np.transpose(img_list[-1],(1,2,0)))
+
+plt.figure(figsize=(10,5))
+plt.title("Measure of Convergence")
+plt.plot(measure_history,label="D_x")
+plt.xlabel("Iterations")
+plt.ylabel("Convergence")
+plt.legend()
+plt.savefig('convergence.png')
